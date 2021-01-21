356e265c91628db03fe32be0f2805bfa80e7fc73
Fahad Sohrab <36382470+fahadsohrab@users.noreply.github.com>
2021-01-21 18:16:20 +0200

    Update OandN_Q.m

--- OandN_Q.m
+++ OandN_Q.m
@@ -1,9 +1,14 @@
-%Orthogonalize and normalize the matrix
+% This fucntion Orthogonalize and normalize the projection matrix Q
+% Input:  Q = The projection matrix
+% Output: Q = Orthogonalize and normalize the projection matrix Q
+
 function Q = OandN_Q(Q)
-% % % % Orthogonalize Q
+%%% Orthogonalize Q
 [tmpQ, ~]=qr(Q',0);  Q = tmpQ';
 clear tmpQ;          clear R;
-% Normalize each projection vector
+
+%%% Normalize each projection vector
 tmpNorm = sqrt(diag(Q*Q'));
 Q = Q./(repmat(tmpNorm',size(Q,2),1)');
+
 end
