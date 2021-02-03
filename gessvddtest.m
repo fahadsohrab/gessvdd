0f195ab0595859ed44b2ea57f7c13e8e284e2bae
Fahad Sohrab <36382470+fahadsohrab@users.noreply.github.com>
2021-02-03 12:38:24 +0200

    Update gessvddtest.m

--- gessvddtest.m
+++ gessvddtest.m
@@ -16,6 +16,21 @@ function varargout=gessvddtest(Testdata,testlabels,gessvddmodel)
 %Example
 %[predicted_labels,accuracy,sensitivity,specificity]=ssvddtest(Testdata,testlabels,ssvddmodel);
 
+%NPT
+    disp('GES-SVDD Testing...')
+    A=gessvddmodel.npt{2};
+    Ktrain=gessvddmodel.npt{3};
+    Phi=gessvddmodel.npt{4};
+    M_train=gessvddmodel.npt{5};
+    NN = size(Testdata,2);
+    N = size(Ktrain,2);
+    Dtest = ((sum(M_train'.^2,2)*ones(1,NN))+(sum(Testdata'.^2,2)*ones(1,N))'-(2*(M_train'*Testdata)));
+    Ktest = exp(-Dtest/A);
+    M = size(Ktest,2);
+    Ktest = (eye(N,N)-ones(N,N)/N) * (Ktest - (Ktrain*ones(N,1)/N)*ones(1,M));
+    Testdata = pinv(Phi')*Ktest;
+%NPT ends
+
 Q=gessvddmodel.Q;
 Model=gessvddmodel.modelparam{end};
 RedTestdata=Q{end}* Testdata;
