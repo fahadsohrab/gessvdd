58bed962ee558d029e75ca16cda4b862b9afbaa6
Fahad Sohrab <36382470+fahadsohrab@users.noreply.github.com>
2021-02-03 15:40:08 +0200

    Update SpectraleigQ.m

--- SpectraleigQ.m
+++ SpectraleigQ.m
@@ -1,12 +1,13 @@
-
 function [Q]=SpectraleigQ(Traindata,J_sepctral,eta,d,L,maxmin)
 
 [evcs, evls] = eig(J_sepctral,L);
+
 if(maxmin==0)%Minimise
     sort_type = 'ascend';
 else %Maximise
     sort_type = 'descend';
 end
+
 evls = diag(evls);     if isreal(evls)==0,    evcs = abs(evcs);  evls = abs(evls);end
 evls(isinf(evls))=0.0; evls(isnan(evls))=0.0; evcs(isinf(evcs))=0.0; evcs(isnan(evcs))=0.0;
 evls(evls<10^-6) = 0.0;
