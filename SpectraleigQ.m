e53fc5385376a4a75576de5f622e1a2023b14a10
Fahad Sohrab <36382470+fahadsohrab@users.noreply.github.com>
2021-02-03 15:05:44 +0200

    Update SpectraleigQ.m

--- SpectraleigQ.m
+++ SpectraleigQ.m
@@ -1,8 +1,12 @@
 
-function [Q]=SpectraleigQ(Traindata,J_sepctral,eta,d,L)
+function [Q]=SpectraleigQ(Traindata,J_sepctral,eta,d,L,maxmin)
 
 [evcs, evls] = eig(J_sepctral,L);
-sort_type = 'ascend'; %ascend descend Change here if you want to sort into ascending or desceding order
+if(maxmin==0)%Minimise
+    sort_type = 'ascend';
+else %Maximise
+    sort_type = 'descend';
+end
 evls = diag(evls);     if isreal(evls)==0,    evcs = abs(evcs);  evls = abs(evls);end
 evls(isinf(evls))=0.0; evls(isnan(evls))=0.0; evcs(isinf(evcs))=0.0; evcs(isnan(evcs))=0.0;
 evls(evls<10^-6) = 0.0;
@@ -10,6 +14,6 @@ evls = nonzeros(evls);
 [~,I] = sort(evls,sort_type);  s_evcs = evcs(:,I);
 T=s_evcs(:,1:d);
 etaI=eta*eye(size(Traindata*Traindata',2));
-W = pinv((Traindata*Traindata')+etaI)*(Traindata*T); 
+W = pinv((Traindata*Traindata')+etaI)*(Traindata*T);
 Q = W';
 end
