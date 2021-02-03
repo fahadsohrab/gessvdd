9417b855d7453dc76c93273645711822f159de8b
Fahad Sohrab <36382470+fahadsohrab@users.noreply.github.com>
2021-02-03 18:11:24 +0200

    Update SpectraleigQ.m

--- SpectraleigQ.m
+++ SpectraleigQ.m
@@ -3,16 +3,21 @@ function [Q]=SpectraleigQ(Traindata,J_sepctral,eta,d,L,maxmin)
 [evcs, evls] = eig(J_sepctral,L);
 
 if(maxmin==0)%Minimise
-    sort_type = 'ascend';
+    factor=+1;
 else %Maximise
-    sort_type = 'descend';
+    factor =-1;
 end
 
 evls = diag(evls);     if isreal(evls)==0,    evcs = abs(evcs);  evls = abs(evls);end
 evls(isinf(evls))=0.0; evls(isnan(evls))=0.0; evcs(isinf(evcs))=0.0; evcs(isnan(evcs))=0.0;
-evls(evls<10^-6) = 0.0;
-evls = nonzeros(evls);
-[~,I] = sort(evls,sort_type);  s_evcs = evcs(:,I);
+
+index = evls<10^-6;
+evls(index)=[];
+evcs(:,index)=[];
+[~, index] = sort(factor*evls);
+evls = evls(index); %Just for future debugging
+s_evcs = evcs(:,index);%
+
 T=s_evcs(:,1:d);
 etaI=eta*eye(size(Traindata*Traindata',2));
 W = pinv((Traindata*Traindata')+etaI)*(Traindata*T);
