6a0a52e2e109f1c3e13f2aa42f71b78b4d3b7c60
Fahad Sohrab <36382470+fahadsohrab@users.noreply.github.com>
2021-02-03 15:04:37 +0200

    Update eigQ.m

--- eigQ.m
+++ eigQ.m
@@ -1,11 +1,14 @@
-function [Q]=eigQ(S_alpha,St,d)
+function [Q]=eigQ(S_alpha,St,d,maxmin)
 [evcs, evls] = eig(S_alpha,St,'qz');
-sort_type = 'ascend'; %% ascend descend Change here if you want to sort into ascending or desceding order
-% check eigenvalues and eigenvectors
+if(maxmin==0)%Minimise
+    sort_type = 'ascend';
+else %Maximise
+    sort_type = 'descend';
+end
+%check eigenvalues and eigenvectors
 evls = diag(evls);     if isreal(evls)==0,    evcs = abs(evcs);  evls = abs(evls);     end
 evls(isinf(evls))=0.0; evls(isnan(evls))=0.0; evcs(isinf(evcs))=0.0; evcs(isnan(evcs))=0.0;
-% select
-
+%select positive and remove small ones
 evls(evls<10^-6) = 0.0;
 evls = nonzeros(evls);
 [~,I] = sort(evls,sort_type);  s_evcs = evcs(:,I);
