a1441dbe841ddec53d0267a4e871ed65a265cc34
Fahad Sohrab <36382470+fahadsohrab@users.noreply.github.com>
2021-02-04 17:03:14 +0200

    Update SpectraleigQ.m

--- SpectraleigQ.m
+++ SpectraleigQ.m
@@ -1,11 +1,11 @@
 function [Q]=SpectraleigQ(Traindata,J_sepctral,eta,d,L,maxmin)
-
+L=L+(0.0001.*ones(size(L)));%Regularization
 [evcs, evls] = eig(J_sepctral,L);
 
 if(maxmin==0)%Minimise
-    factor=+1;
+    factor=+1; %Ascending
 else %Maximise
-    factor =-1;
+    factor =-1; %Descending
 end
 
 evls = diag(evls);     if isreal(evls)==0,    evcs = abs(evcs);  evls = abs(evls);end
@@ -15,7 +15,7 @@ index = evls<10^-6;
 evls(index)=[];
 evcs(:,index)=[];
 [~, index] = sort(factor*evls);
-evls = evls(index); %Just for future debugging
+evls = evls(index);%Just for future debugging
 s_evcs = evcs(:,index);%
 
 T=s_evcs(:,1:d);
