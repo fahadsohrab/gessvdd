73f090ad7515faf838af0d1e65c6ef42e3ae4e7d
Fahad Sohrab <36382470+fahadsohrab@users.noreply.github.com>
2021-02-04 18:19:25 +0200

    Update SpectraleigQ.m

--- SpectraleigQ.m
+++ SpectraleigQ.m
@@ -1,5 +1,5 @@
 function [Q]=SpectraleigQ(Traindata,J_sepctral,eta,d,L,maxmin)
-L=L+(0.0001.*ones(size(L)));%Regularization
+L=L+(0.0001.*eyes(size(L)));%Regularization
 [evcs, evls] = eig(J_sepctral,L);
 
 if(maxmin==0)%Minimise
