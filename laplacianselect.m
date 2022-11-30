57f330ef235a3beb514d04565c5292806587a3ea
Fahad Sohrab <36382470+fahadsohrab@users.noreply.github.com>
2022-11-30 14:37:52 +0200

    Update laplacianselect.m

--- laplacianselect.m
+++ laplacianselect.m
@@ -1,61 +1,35 @@
-function L=laplacianselect(train,laptype,knngmean,gamma)
+function L = laplacianselect(Traindata, params)
 
-if laptype==1
-    noOfTrainData=size(train,2);
-    L =  (1/noOfTrainData)*(eye(noOfTrainData) - 1/noOfTrainData *(ones(noOfTrainData,1)*ones(noOfTrainData,1)'));
-    
-elseif laptype==2
-    clusters=knngmean;
-    idx = kmeans(train',clusters);
-    A = 0 ;
-    for clustersnumber=1:clusters
-        temp = idx==clustersnumber;
-        temp=double(temp);
-        current_matrix = temp*temp';
-        current_matrix = current_matrix./ sum(temp) ;
-        A = A + current_matrix;
-    end
-    L=eye(size(A))-A;
-    
-elseif laptype==3
-    N = size(train,2);
-    Dtrain = ((sum(train'.^2,2)*ones(1,N))+(sum(train'.^2,2)*ones(1,N))'-(2*(train'*train)));
-    sigma2 = gamma * mean(mean(Dtrain));  A = 2.0 * sigma2;
-    Ktrain = exp(-Dtrain/A);
-    %Ktrain = KforKnn;
-    W = zeros(size(Ktrain));
-    kNN = knngmean;
-    
-    for ii=1:size(Ktrain,2)
-        [~, ind] = sort(Ktrain(ii,:),'descend');
-        W(ii,ind(1:kNN)) = 1.0;
-    end
-    W=(W+W');
-    W(W~=0)=1.0;
-    D = sum(W')';
-    L = diag(D) - W;
-    
-elseif laptype==4%S_B
-    clusters=knngmean;
-    idx = kmeans(train',clusters);
-    A = 0 ;
-    for clustersnumber=1:clusters
-        temp = idx==clustersnumber;
-        temp=double(temp);
-        Nc=sum(temp);
-        temp=temp./Nc;
-        tempones=ones(size(temp));
-        tempones=tempones./sum(tempones);
-        temp=temp-tempones;
-        current_matrix=temp*temp';
-        current_matrix=Nc.*current_matrix;
-        A = A + current_matrix;
-    end
-    L=A;
-   
-else
-    msg = 'Error: Input a correct selection for laptype, 1 for PCA, 2 for S_w, 3 for knn, 4 for S_b';
-    error(msg)
-end
-
+N = size( Traindata, 2 );
+switch params.variant
+    case 'ellipsoid'
+        L = eye(N);
+    case 'pca'
+        L =  (1/N)*(eye(N) - 1/N *(ones(N,1)*ones(N,1)'));  
+    case 'kNN'
+        Dtrain = ((sum(Traindata'.^2,2)*ones(1,N))+(sum(Traindata'.^2,2)*ones(1,N))'-(2*(Traindata'*Traindata)));
+        W = zeros(N);
+        for ii=1:N
+            [~, ind] = sort(Dtrain(ii,:),'descend');
+            W(ii,ind(1:params.K)) = 1.0;
+        end
+        W=(W+W'); %Make symmetric
+        W(W~=0)=1.0;
+        D = sum(W')'; 
+        L = diag(D) - W;
+    case 'Sw'
+        idx = kmeans(Traindata',params.K);
+        A = zeros(N);
+        for c=1:params.K
+            Nc = sum(idx==c);
+            A = A + double(idx==c)*double(idx==c)'/Nc;
+        end
+        L=eye(N)-A;
+    case 'Sb'
+        idx = kmeans(Traindata',params.K);
+        L = 0 ;
+        for c=1:params.K
+            Nc = sum(idx==c);
+            L = L + Nc*(double(idx==c)/Nc - ones(N)/N)*(double(idx==c)/Nc - ones(N)/N)';
+        end
 end
