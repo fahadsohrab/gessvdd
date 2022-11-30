128f8edc1e3a519b748874784af5a87b4be070c0
Fahad Sohrab <36382470+fahadsohrab@users.noreply.github.com>
2022-11-30 14:38:17 +0200

    Update sortEigVecs.m

--- sortEigVecs.m
+++ sortEigVecs.m
@@ -1,29 +1,28 @@
+function [mevcs, mevls] = sortEigVecs(evcs,evls,type)
 % Input:
 % evls: not sorted eigenvalues (row vector)
 % evs: not sorted eigenvactors (columns)
-% m: number of eigenvectors/eigenvalues to keep
 %
 % Output:
-% mevcs: maximun eigenvectors in descending order
-% mevls maximun eigenvalues in descending order
-function [mevcs, mevls] = sortEigVecs(evs,evls,type)
+% sorted eigenvalues and eigenvectors
 
 if nargin < 3,  type = 'descend';  end
-nans_found = sum(isnan(evls));      complex_found = sum( imag(evls) ~=0 ); 
-if nans_found~=0,    evls(isnan(evls)) = min(evls)*10^(-1)*ones(nans_found,1);       end % replace NANs with smallest value
-if complex_found~=0, evls(imag(evls)~=0) = min(evls)*10^(-1)* ones(complex_found,1); end % replace complex numbers with smallest value
+ 
+evls(imag(evls)~=0) = 0; 
+evcs(imag(evcs)~=0) = real(evcs(imag(evcs)~=0));    
 
-nans_found = sum(isnan(evs));      complex_found = sum( imag(evs) ~=0 ); 
-if nans_found~=0,    evs(isnan(evs)) = 0;     end % replace NANs with smallest value
-if complex_found~=0, evs(imag(evs)~=0) = 0;   end % replace complex numbers with smallest value
+evls(isinf(evls))=0.0; 
+evls(isnan(evls))=0.0; 
+evcs(isinf(evcs))=0.0; 
+evcs(isnan(evcs))=0.0;
 
+%Round near-zero eigenvalues to zero
+evls(evls<10^-6) = 0.0;
 
-% sort eigenvalues
-[auxv, auxi] = sort(evls,type);     clear auxv; 
-
-% sort eigenvectors
-for i=1:length(evls);
-    idx = auxi(i); % get index of ith eigenvalue 
-    mevls(:,i) = evls(idx); % get ith eigenvalue
-    mevcs(:,i) = evs(:,idx); % get ith eigenvector
+%Sort eigenvalues and remove zeros
+[auxv, auxi] = sort(evls,type);
+mevls = auxv;
+mevcs = evcs(:,auxi);
+mevcs = mevcs(:,mevls > 0);
+mevls = mevls(mevls > 0);
 end
