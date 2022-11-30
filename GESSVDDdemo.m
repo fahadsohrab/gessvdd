5e0ff49aae2d963c2a9ce9eb3be2cf687bd9a42f
Fahad Sohrab <36382470+fahadsohrab@users.noreply.github.com>
2022-11-30 14:25:34 +0200

    Update GESSVDDdemo.m

--- GESSVDDdemo.m
+++ GESSVDDdemo.m
@@ -4,32 +4,45 @@ clc
 close all
 clear
 
-%%Generate Random Data
+%% Possible inputs to gessvddtrain
+% The first input argument is the Traindata (target training data)
+% other inputs/options are
+%
+% params.variant  :Possible options are 'basic', 'ellipsoid', 'pca', 'kNN', 'Sw', 'Sb'. Default= 'pca'
+% params.solution :Possible options are 'gradient', 'eig', 'spectral_regression' Default='gradient'
+% params.C        :Value of hyperparameter C, Default=0.1.
+% params.d        :Data in lower dimension, make sure that params.dim<D, Default=2.
+% params.eta      :Needed only with gradient solution, Used as step size for gradient, Default=0.01.
+% params.npt      :Used for selecting non-linear data description. Possible options are 1 (for non-linear data description), default=1 (linear data description)
+% params.s        :Hyperparameter for the kernel, used in non-linear data description. Default=10.
+% params.K:       :Number of clusters (S_w,S_b), Number of K-neighbors(knn),Default=5.
+% params.minmax   :Possible options are 'max', 'min' ,Default='min'.
+% params.maxIter  :Maximim iteraions of the algorithm. Default=10.
+
+%% Generate Random Data
 noOfTrainData = 500; noOfTestData = 100;
-D= 5; %Original dimentionality of data
-Traindata = rand(D,noOfTrainData); %Training Data/Features
+D= 5; %D=Original dimensionality of data/features
+Traindata = rand(D,noOfTrainData); %Training data/features
 %Training labels (all +1s) are not needed.
 
 testlabels = -ones(noOfTestData,1);
 perm = randperm(noOfTestData);
 positiveSamples = floor(noOfTestData/2);
 testlabels(perm(1:positiveSamples))=1; % test labels, +1 for target, -1 for outliers
-Testdata= rand(D,noOfTestData); %Testing Data/Features from modality 1
+Testdata= rand(D,noOfTestData); %Testing data/features
 
-%Possible inputs to essvddtrain
-% The first input argument is the Training (target) data
-%other options are
-%   'maxIter' :Maximim iteraions, Default=100
-%   'C'       :Value of hyperparameter C, Default=0.1
-%   'd'       :Data in lower dimension, make sure that input d<D, Default=1,
-%   'eta'     :Used as step size for gradient, Default=0.1
-%   'opt'     :Selection of optimisation type, Default=3 (Spectral regression based)
-%              other options: 1=Gradient Based Solution, 2=Generalized eigen value based
-%   'laptype' :Selection for laplacian type, 1 for PCA, 2 for S_w, 3 for knn, 4 for S_b
-%   'L'       :User's defined Laplacian matrix
-%   's'       :Hyperparameter for the kernel. 
-%   'kcluster':Number of clusters (S_w,S_b), Number of K-neighbors(knn),Default=5
-%   'max'     :Input 1 for maximisation, (Default=0, minimization)
+%% Input parameters setting example
+params.variant = 'pca';
+params.solution = 'gradient';
+params.minmax = 'max';
+params.maxIter = 5;
+params.Cval=0.5;
+params.d=2;
+params.eta=0.2;
+params.npt=1;
+params.s=5;
+params.maxIter = 10;
 
-gessvddmodel=gessvddtrain(Traindata,'C',0.1,'d',4,'maxIter',10,'opt',3,'laptype',4,'max',1);
-[predicted_labels,accuracy,sensitivity,specificity]=gessvddtest(Testdata,testlabels,gessvddmodel);
+%% Training and Testing
+gessvddmodel=gessvddtrain(Traindata,params);
+[predicted_labels,eval]=gessvddtest(Testdata,testlabels,gessvddmodel);
